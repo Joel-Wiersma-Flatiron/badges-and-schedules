5bd4b60e15c743a9460d54a15ad60d967f4de5e5
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-10-27 16:24:48 -0600

    commit tree 0064e72a70440d7d0e1232d1bdf50d7d28f8d96d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new_array = []
+    array.each do |name|
+        new_array.push(badge_maker(name))
+    end
+    return new_array
+end
+
+def assign_rooms(array)
+    room_number = 1
+    array.each do |speaker|
+        array[room_number - 1] = "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+        room_number += 1
+    end
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |name|
+        puts "#{name}"
+    end
+    assign_rooms(array).each do |room|
+        puts "#{room}"
+    end
+end
